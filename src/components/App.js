712de343b01d23cedf10a92c42c2cc3cf1db2537
Unknown <christopher.nyberg@gmail.com>
2017-03-27 15:07:52 -0700

    Changed to 2 spaces.

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,17 @@ import Order from './Order'
 import Inventory from './Inventory'
 
 class App extends React.Component {
-    render() {
-        return (
-            <div className="catch-of-the-day">
-                <div className="menu">
-                    <Header tagline="Fresh Seafood Market"/>
-                </div>
-                <Order />
-                <Inventory />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+        </div>
+        <Order />
+        <Inventory />
+      </div>
+    )
+  }
 }
 
 export default App;
